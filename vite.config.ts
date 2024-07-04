acd57d6867b029f0155242a12ec50692a3827b00
hayato2158 <k.hayato2158@gmail.com>
2024-07-04 19:53:52 +0900

    aaaa

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES ? "omron-sensor-graph-Vite" : "./",
+  base: process.env.GITHUB_PAGES ? "omron-sensor-graph-vite" : "./",
 })
